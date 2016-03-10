240252f822225ff9bcebd6a4ee172b82399cf1a4
Sandeep <sandeep.sajiru@gmail.com>
2016-03-10 18:59:33 +0530

    Insertion Code Done

--- Sandeep/BST - 1/BST.c
+++ Sandeep/BST - 1/BST.c
@@ -12,12 +12,40 @@ BSTNode *createBSTNode(ElementType data)
 BSTNode *insertBSTNode(BSTNode *root, ElementType data)
 {
     // If root== NULL , new node should be returned
+    BSTNode *t = createBSTNode(data);
 
+    BSTNode *originalRoot = root;
+    if(root==NULL)
+        return t;
+    else
+    {
+        while(1)
+        {
 
+            if(data <= root->data)
+            {
+                if(root->left==NULL)
+                {
+                    root->left=t;
+                    break;
+                }
+                root = root->left;
+            }
+            else
+            {
+                if(root->right==NULL)
+                {
+                    root->right = t;
+                    break;
+                }
+                root = root->right;
+            }
+        }
+    }
     // Find a parent under which new node can be inserted
 
 
-    return NULL;
+    return originalRoot;
 }
 
 BSTNode *rinsertBSTNode(BSTNode *root, ElementType data)
